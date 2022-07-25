0044c16e53be3cc7262d68bb79a288ae1a2b98af
Mariano Biagioli <mariano_biagioli@live.com.ar>
2022-07-25 20:37:22 -0300

    actualizacion de modelos

--- App/models.py
+++ App/models.py
@@ -5,23 +5,28 @@ from ckeditor.fields import RichTextField
 
 
 class Usuario(models.Model):
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    user = models.CharField(max_length=20)
+    t_usuario = (("1", "Administrador"), ("2", "Docente"))
+    tipo_de_usuario = models.CharField(max_length = 20, choices = t_usuario, default = "1")
     avatar = models.ImageField(upload_to="avatars", null=True, blank=True)
+    nombre_apellido = models.CharField()
     email = models.EmailField()
     celular = models.IntegerField()
-    descripcion = RichTextField()
+    descripcion_docente = RichTextField()
+    provincia = models.CharField(max_length=20)
+    pais = models.CharField(max_length=20) #AGREGAR LISTADO DE PAISES
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.user.username}"
+        return f"{self.Usuario.user}"
 
 
 class Anuncio(models.Model):
     titulo = models.CharField(max_length=100)
     materia = models.CharField(max_length=180)
     autor = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
-    descripcion_docente = models.ForeignKey(Usuario.descripcion)
+    descripcion_docente_a = models.ForeignKey(Usuario.descripcion_docente)
     avatar_docente = models.ForeignKey(Usuario.avatar)
     imagen = models.ImageField(upload_to="articles", null=True, blank=True)
     contacto_mail = models.ForeignKey(Usuario.email)
